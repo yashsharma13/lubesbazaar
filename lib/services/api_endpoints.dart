c17f0a0664536e7d9f1ffea4127225fa1245001a
yashsharma13 <ys686722@gmail.com>
2025-09-14 15:35:15 +0530

    Update API endpoints

--- lib/services/api_endpoints.dart
+++ lib/services/api_endpoints.dart
@@ -17,20 +17,3 @@ class ApiEndpoints {
   // 🔹 Orders APIs (future me)
   static String orders = "$baseDomain/orders";
 }
-
-// class ApiEndpoints {
-//   static const String baseDomain = "http://192.168.1.43:8000/api/v3";
-//   // http://3.138.188.164/api/v3/product
-
-//   // 🔹 User/Auth related APIs
-//   static const String user = "$baseDomain/user";
-
-//   // 🔹 Menu APIs
-//   static const String menu = "$baseDomain/menu";
-
-//   // 🔹 Product APIs
-//   static const String product = "$baseDomain/product";
-
-//   // 🔹 Orders APIs (future me)
-//   static const String orders = "$baseDomain/orders";
-// }
